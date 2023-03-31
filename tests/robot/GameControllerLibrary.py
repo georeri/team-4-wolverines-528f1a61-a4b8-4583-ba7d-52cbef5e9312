85f5e3319ea7283b8c5ac6a014ebf1fce8bc9d27
Eric Van Wagner <eric.vanwagner@gmail.com>
2023-03-31 19:11:18 +0000

    added game controller

--- tests/robot/GameControllerLibrary.py
+++ tests/robot/GameControllerLibrary.py
@@ -2,6 +2,19 @@ from levelup.controller import GameController
 
 
 class GameControllerLibrary:
+
+    start_x: int
+    start_y: int
+    move_count: int
+
+    def initialize_character_xposition_with(self, x_position):
+        self.start_x = x_position 
+
+    def initialize_character_yposition_with(self, y_position):
+        self.start_y = y_position
+
+    def initialize_character_moveCount_with(self, move_count):
+        self.move_count = move_count
     def initialize_controller(self):
         pass
 
@@ -12,13 +25,20 @@ class GameControllerLibrary:
         pass
 
     def number_of_map_positions_should_be(self, expected):
-        raise AssertionError("Not implemented")
+        self.controller = GameController()
+        self.controller.create_character("Eric")
+        self.controller.start_game()
+        if self.controller.map.get_size() != int(expected):
+            raise AssertionError(int(self.controller.map.get_size()), int(expected))
 
     def starting_X_coordinate_should_be(self, expected):
-        raise AssertionError("Not implemented")
+        if int(self.controller.character.getposition().coordinates[0]) != int(expected):
+            raise AssertionError(int(self.controller.character.getposition().coordinates[0]), int(expected))
 
     def starting_Y_coordinate_should_be(self, expected):
-        raise AssertionError("Not implemented")
+        if int(self.controller.character.getposition().coordinates[1]) != int(expected):
+            raise AssertionError(int(self.controller.character.getposition().coordinates[1]), int(expected))
 
     def starting_move_count_should_be(self, expected):
-        raise AssertionError("Not implemented")
+         if int(self.controller.status.move_count) != int(expected):
+            raise AssertionError(int(self.controller.status.move_count), int(expected))
